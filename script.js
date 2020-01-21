2b02526292f5727767f5830ee70118695e0cf6e7
Jwhitemocha <jtvmo4@gmail.com>
2020-01-20 21:14:48 -0800

    finished javascript for button functions

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
 // Create Const for ID's
 const resultEl = document.getElementById("result");
-const lengthEL = document.getElementById("length");
+const lengthEl = document.getElementById("length");
 const uppercaseEl = document.getElementById("uppercase");
 const lowercaseEl = document.getElementById("lowercase");
 const numbersEl = document.getElementById("numbers");
@@ -12,9 +12,71 @@ const clipboard = document.getElementById("clipboard");
 const randomFunction = { 
     lower: getRandomLower, 
     upper: getRandomUpper,
-    number: getRandomNumber,
+    numbers: getRandomNumber,
     special: getRandomSpecial
+};
+
+generateEl.addEventListener("click",() => { 
+    const length = +lengthEl.value;
+    const hasLower = lowercaseEl.checked;
+    const hasUpper = uppercaseEl.checked;
+    const hasNumbers = numbersEl.checked;
+    const hasSpecial = specialEl.checked;
+
+    resultEl.innerText = generatePassword(hasLower, hasUpper, hasNumbers, hasSpecial, length);
+});
+//create password button make it work
+function generatePassword(lower, upper, numbers, special, length) { 
+    let generatedPassword = " ";
+    const typesCount = lower + upper + numbers + special; 
+    const typesArr = [{lower}, {upper}, {numbers}, {special}].filter
+    (
+        item => Object.values(item)[0]
+    );
+
+        if(typesCount ===0) { 
+            return " ";
+        }
+        for(let i=0; i<length; i+=typesCount) { 
+            typesArr.forEach(type => { 
+                const functionName = Object.keys(type)[0];
+                generatedPassword += randomFunction[functionName]();
+            });
+        }
+
+        const finalPassword = generatedPassword.slice(0,length);
+        return finalPassword;
+}
+console.log(generatePassword);
+
+// make functions for the checkboxes
+function getRandomLower() { 
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
 }
 
+
+function getRandomUpper() { 
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+function getRandomNumber() { 
+    return +String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+function getRandomSpecial() { 
+    const special = "!@#$%^&*()=[]<>.,/'"
+    return special[Math.floor(Math.random() * special.length)];
+}
+console.log(getRandomSpecial());
+// the clipboard icon button to copy password
+clipboard.addEventListener("click", () => { 
+    const textarea = document.createElement("textarea");
+    const password = resultEl.innerText;
+    if(!password) { return; }
+    textarea.value = password; 
+    document.body.appendChild(textarea);
+    textarea.select();
+    document.execCommand("copy");
+    textarea.remove();
+    alert("Copied Password");
+});
 
 
